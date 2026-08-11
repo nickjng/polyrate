91984759a21140aa6fb2bcece113b0f5c3cb74fd
nickjng <nickolas.ng@outlook.com>
2026-08-11 00:09:29 -0300

    feat: first commit, init project

---
+++ back-poly/src/main/java/nick/project/polyrate/PolyrateApplication.java
@@ -0,0 +1,13 @@
+package nick.project.polyrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyrateApplication.class, args);
+	}
+
+}
---
+++ back-poly/src/test/java/nick/project/polyrate/PolyrateApplicationTests.java
@@ -0,0 +1,13 @@
+package nick.project.polyrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
